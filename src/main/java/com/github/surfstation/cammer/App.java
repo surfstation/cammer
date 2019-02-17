f1a54b98e24f3c387b4aa1878143eba9a593aa03
surfstation <2008147+surfstation@users.noreply.github.com>
2019-02-16 17:58:48 -1000

    v2

--- src/main/java/com/github/surfstation/cammer/App.java
+++ src/main/java/com/github/surfstation/cammer/App.java
@@ -33,12 +33,8 @@ import com.google.api.services.youtube.model.LiveBroadcastContentDetails;
 import com.google.api.services.youtube.model.LiveBroadcastListResponse;
 import com.google.api.services.youtube.model.LiveBroadcastSnippet;
 import com.google.api.services.youtube.model.LiveBroadcastStatus;
-import com.google.api.services.youtube.model.PlaylistItem;
-import com.google.api.services.youtube.model.PlaylistItemListResponse;
-import com.google.api.services.youtube.model.PlaylistItemSnippet;
-import com.google.api.services.youtube.model.ResourceId;
 
-// 12 hour archive limit: https://support.google.com/youtube/answer/6247592?hl=en
+// 12 hour archive limit: https://support.google.com/youtube/answer/6247592
 // https://developers.google.com/youtube/v3/
 // https://developers.google.com/youtube/v3/code_samples/java
 // https://developers.google.com/youtube/v3/docs/playlists/update
@@ -68,39 +64,14 @@ public class App {
     transitionActiveBroadcastsToCompleted(youtube_surfstationcam);
     final String broadcastId = createBroadcast(youtube_surfstationcam);
     Files.write(Paths.get(args[2]), ("SurfStation.youtube_video_callback_surfstationcam({ \"videoId\": \"" + broadcastId + "\" });\n").getBytes(StandardCharsets.UTF_8));
-    //new ProcessBuilder("systemctl", "stop", "youtube-live-stream.service").start().waitFor(60, TimeUnit.SECONDS);
-    //Thread.sleep(5000L);
-    //new ProcessBuilder("systemctl", "start", "youtube-live-stream.service").start().waitFor(60, TimeUnit.SECONDS);
     deletePriorLiveBroadcasts(youtube_surfstationcam); // this deletes the archived videos and we want to keep those for later viewing for some amount of time
-    // will stop doing this later because i am not going to support using the playlist anymore
-    clearPlaylist(youtube_surfstationcam, "PLJyLRh0ulkUdaPUMV3J_pA_OLphbxDGvJ");
-    addBroadcastToPlaylist(youtube_surfstationcam, broadcastId, "PLJyLRh0ulkUdaPUMV3J_pA_OLphbxDGvJ");
-  }
-
-  public static List<String> clearPlaylist(final YouTube youtube, final String playlistId) throws IOException {
-    final List<String> result = new LinkedList<String>();
-    PlaylistItemListResponse items = null;
-    do {
-      final String pageToken = (null == items) ? null : items.getNextPageToken();
-      items = youtube.playlistItems().list("id").setMaxResults(50L).setPlaylistId(playlistId).setPageToken(pageToken).execute();
-      for (final PlaylistItem item : items.getItems()) {
-        try {
-          youtube.playlistItems().delete(item.getId()).execute();
-          result.add(item.getId());
-        } catch (final Exception e) {
-          System.out.println(e.getMessage());
-        }
-      }
-    } while (items.getNextPageToken() != null);
-    return result;
   }
 
   public static List<String> transitionActiveBroadcastsToCompleted(final YouTube youtube) throws IOException {
     final List<String> result = new LinkedList<String>();
-    LiveBroadcastListResponse broadcasts = null;
+    LiveBroadcastListResponse broadcasts = new LiveBroadcastListResponse();
     do {
-      final String pageToken = (null == broadcasts) ? null : broadcasts.getNextPageToken();
-      broadcasts = youtube.liveBroadcasts().list("id").setBroadcastStatus("active").setMaxResults(50L).setPageToken(pageToken).execute();
+      broadcasts = youtube.liveBroadcasts().list("id").setBroadcastStatus("active").setMaxResults(50L).setPageToken(broadcasts.getNextPageToken()).execute();
       for (final LiveBroadcast broadcast : broadcasts.getItems()) {
         try {
           result.add(youtube.liveBroadcasts().transition("complete", broadcast.getId(), "id").execute().getId());
@@ -114,10 +85,9 @@ public class App {
 
   public static List<String> deletePriorLiveBroadcasts(final YouTube youtube) throws IOException {
     final List<String> result = new LinkedList<String>();
-    LiveBroadcastListResponse broadcasts = null;
+    LiveBroadcastListResponse broadcasts = new LiveBroadcastListResponse();
     do {
-      final String pageToken = (null == broadcasts) ? null : broadcasts.getNextPageToken();
-      broadcasts = youtube.liveBroadcasts().list("id,snippet").setBroadcastStatus("completed").setMaxResults(50L).setPageToken(pageToken).execute();
+      broadcasts = youtube.liveBroadcasts().list("id,snippet").setBroadcastStatus("completed").setMaxResults(50L).setPageToken(broadcasts.getNextPageToken()).execute();
       for (final LiveBroadcast broadcast : broadcasts.getItems()) {
         try {
           final long now = new Date().getTime();
@@ -135,40 +105,13 @@ public class App {
   }
 
   public static String createBroadcast(final YouTube youtube) throws IOException {
-    final LiveBroadcastContentDetails details = new LiveBroadcastContentDetails();
-    details.setEnableAutoStart(true);
-    details.setEnableDvr(true);
-    details.setEnableEmbed(true);
-    details.setEnableLowLatency(true);
-    details.setLatencyPreference("low");
-    details.setEnableClosedCaptions(false);
-    final LiveBroadcastSnippet snippet = new LiveBroadcastSnippet();
     final Instant now = Instant.now();
     final String datetime = DateTimeFormatter.ofPattern("EEE LLL dd hh:mma").withZone(ZoneId.of("America/New_York")).format(now);
-    snippet.setTitle("The Surf Station 3rd St Wavecam - " + datetime);
-    snippet.setDescription("St. Augustine, FL 32080. Recording started at about " + datetime + ".");
-    snippet.setScheduledStartTime(new DateTime(now.getEpochSecond() * 1000L));
-    snippet.setScheduledEndTime(new DateTime(now.getEpochSecond() * 1000L));
-    final LiveBroadcastStatus status = new LiveBroadcastStatus();
-    status.setPrivacyStatus("public");
-    final LiveBroadcast broadcast = new LiveBroadcast();
-    broadcast.setKind("youtube#liveBroadcast");
-    broadcast.setSnippet(snippet);
-    broadcast.setStatus(status);
-    broadcast.setContentDetails(details);
+    final LiveBroadcastContentDetails details = new LiveBroadcastContentDetails().setEnableAutoStart(true).setEnableDvr(true).setEnableEmbed(true).setEnableLowLatency(true).setLatencyPreference("low").setEnableClosedCaptions(false);
+    final LiveBroadcastSnippet snippet = new LiveBroadcastSnippet().setTitle("The Surf Station 3rd St Wavecam - " + datetime).setDescription("St. Augustine, FL 32080. Recording started at about " + datetime + ".").setScheduledStartTime(new DateTime(now.getEpochSecond() * 1000L)).setScheduledEndTime(new DateTime(now.getEpochSecond() * 1000L));
+    final LiveBroadcastStatus status = new LiveBroadcastStatus().setPrivacyStatus("public");
+    final LiveBroadcast broadcast = new LiveBroadcast().setKind("youtube#liveBroadcast").setSnippet(snippet).setStatus(status).setContentDetails(details);
     final LiveBroadcast result = youtube.liveBroadcasts().insert("snippet,contentDetails,status", broadcast).execute();
     return youtube.liveBroadcasts().bind(result.getId(), "id").setStreamId("GKOoJhHZiQhDyfvaZ8aAYw1548094394731553").execute().getId();
-  }
-
-  public static void addBroadcastToPlaylist(final YouTube youtube, final String broadcastId, final String playlistId) throws IOException {
-    final ResourceId resourceId = new ResourceId();
-    resourceId.set("kind", "youtube#video");
-    resourceId.set("videoId", broadcastId);
-    final PlaylistItemSnippet snippet = new PlaylistItemSnippet();
-    snippet.setResourceId(resourceId);
-    snippet.setPlaylistId(playlistId);
-    final PlaylistItem playlistItem = new PlaylistItem();
-    playlistItem.setSnippet(snippet);
-    youtube.playlistItems().insert("snippet", playlistItem).execute();
   }
 }
